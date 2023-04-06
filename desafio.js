e042989b1c6679bd17aa3823ab9ac22922048a62
Fernando Bruno <fernandobrunoslima@gmail.com>
2023-04-06 10:45:53 -0300

    Desafio finalizado

--- desafio.js
+++ desafio.js
@@ -1,21 +1,15 @@
-// prompt para iniciar jogo
 let startRound = confirm(
   `~ Boas vindas ao jogo Blackjack! ~\nQuer iniciar uma rodada?`
 );
-
-//se negativo
-if (!startRound) {
-  alert(`\n~ Que pena, volte pra jogar quando quiser ~`);
-  //se positivo
-} else {
-  // fazer loop enquanto starRound for positivo
-  do {
+while (startRound) {
+  if (startRound) {
+    // variaveis para salvar dados de usuário e computador
     let userCards = [];
     let userScore = 0;
     let computerCards = [];
     let computerScore = 0;
 
-    // solicitar duas cartas para cada; se tiver dois As, reinicia o jogo
+    // codigo para primeira jogada, onde cada jogador recebe duas cartas; com a regra de repetir jogada se um deles receber dois A's
     do {
       userCards = [];
       userScore = 0;
@@ -37,8 +31,9 @@ if (!startRound) {
       (userCards[0] === "A" && userCards[1] === "A") ||
       (computerCards[0] === "A" && computerCards[1] === "A")
     );
+    // encerramento de codigo de primeira rodada alimentando variaveis fora do escopo desse 'do' acima
 
-    // mostrar mensagem após primeiras rodadas automáticas sobre se continua
+    // mensagem perguntando se deseja continuar pedindo cartas
     let message = `Suas cartas são: ${userCards.join(
       " "
     )}.\nA carta revelada do computador é ${
@@ -48,8 +43,50 @@ if (!startRound) {
     let continueRound = confirm(message);
     let roundEnded = false;
 
-    // se continuar round for negativo entra no if para encerrar com resultado
-    if (!continueRound) {
+    // se desejar continuar fazer loop com compras de cartas
+    if (continueRound) {
+      while (continueRound && !roundEnded) {
+        let card = comprarCarta();
+        userCards.push(card.texto);
+        userScore += card.valor;
+        card = comprarCarta();
+        computerCards.push(card.texto);
+        computerScore += card.valor;
+
+        if (userScore > 21 && computerScore > 21) {
+          roundEnded = true;
+          message = `Usuário - Cartas: ${userCards.join(
+            " "
+          )} - Pontuação: ${userScore}\nComputador - Cartas: ${computerCards.join(
+            " "
+          )} - Pontuação: ${computerScore}\nEmpate!`;
+          alert(message);
+        } else if (userScore > 21) {
+          roundEnded = true;
+          message = `Usuário - Cartas: ${userCards.join(
+            " "
+          )} - Pontuação: ${userScore}\nComputador - Cartas: ${computerCards.join(
+            " "
+          )} - Pontuação: ${computerScore}\nO computador ganhou!`;
+          alert(message);
+        } else if (computerScore > 21) {
+          roundEnded = true;
+          message = `Usuário - Cartas: ${userCards.join(
+            " "
+          )} - Pontuação: ${userScore}\nComputador - Cartas: ${computerCards.join(
+            " "
+          )} - Pontuação: ${computerScore}\nO usuário ganhou!`;
+          alert(message);
+        } else {
+          message = `Suas cartas são: ${userCards.join(
+            " "
+          )}.\nA carta revelada do computador é ${
+            computerCards[0]
+          }\nDeseja comprar mais uma carta?`;
+          continueRound = confirm(message);
+        }
+      }
+    } else {
       if (userScore > computerScore) {
         roundEnded = true;
         message = `Usuário - Cartas: ${userCards.join(
@@ -74,44 +111,11 @@ if (!startRound) {
           " "
         )} - Pontuação: ${computerScore}\nEmpate!`;
         alert(message);
-
-        // se continuar for positivo entrar nesse else para continuar comprando cartas
-      } else {
-        while (continueRound && !roundEnded) {
-          const card = comprarCarta();
-          userCards.push(card.texto);
-          userScore += card.valor;
-          card = comprarCarta();
-          computerCards.push(card.texto);
-          computerScore += card.valor;
-
-          if (userScore > 21) {
-            roundEnded = true;
-            message = `Usuário - Cartas: ${userCards.join(
-              " "
-            )} - Pontuação: ${userScore}\nComputador - Cartas: ${computerCards.join(
-              " "
-            )} - Pontuação: ${computerScore}\nO computador ganhou!`;
-            alert(message);
-          } else if (computerScore > 21) {
-            roundEnded = true;
-            message = `Usuário - Cartas: ${userCards.join(
-              " "
-            )} - Pontuação: ${userScore}\nComputador - Cartas: ${computerCards.join(
-              " "
-            )} - Pontuação: ${computerScore}\nO usuário ganhou!`;
-            alert(message);
-          } else {
-            message = `Suas cartas são: ${userCards.join(
-              " "
-            )}.\nA carta revelada do computador é ${
-              computerCards[0]
-            }\nDeseja comprar mais uma carta?`;
-            continueRound = confirm(message);
-          }
-        }
       }
     }
-  } while (startRound);
-  startRound = confirm("Quer iniciar uma nova rodada?");
+    message = `Deseja jogar mais uma rodada?`;
+    startRound = confirm(message);
+  } else {
+    alert(`\n~ Que pena, volte pra jogar quando quiser ~`);
+  }
 }
